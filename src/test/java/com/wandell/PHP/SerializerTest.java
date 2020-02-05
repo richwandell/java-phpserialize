3c92f900b4f21b60f92ff0070091c11aefeb647d
Rich Wandell <rich.wandell@etouches.com>
2020-02-05 14:47:30 -0500

    second

--- src/test/java/com/wandell/PHP/SerializerTest.java
+++ src/test/java/com/wandell/PHP/SerializerTest.java
@@ -3,13 +3,11 @@ package com.wandell.PHP;
 import com.wandell.PHP.pojo.Pojo1;
 import com.wandell.PHP.pojo.Pojo2;
 import org.junit.jupiter.api.Test;
+
+import static com.wandell.PHP.PHP.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SerializerTest {
-
-    private static final String SIMPLE_OBJECT = "O:26:\"wandell\\Example\\Popo\\Popo1\":7:{s:1:\"a\";N;s:1:\"b\";i:0;s:29:\"\0wandell\\Example\\Popo\\Popo1\0c\";N;s:4:\"\0*\0d\";i:0;s:1:\"e\";N;s:29:\"\0wandell\\Example\\Popo\\Popo1\0f\";N;s:4:\"\0*\0g\";b:0;}";
-    private static final String OBJECT_WITH_REFERENCE = "O:26:\"wandell\\Example\\Popo\\Popo2\":2:{s:35:\"\0wandell\\Example\\Popo\\Popo2\0object1\";O:26:\"wandell\\Example\\Popo\\Popo1\":7:{s:1:\"a\";N;s:1:\"b\";i:0;s:29:\"\0wandell\\Example\\Popo\\Popo1\0c\";N;s:4:\"\0*\0d\";i:0;s:1:\"e\";N;s:29:\"\0wandell\\Example\\Popo\\Popo1\0f\";N;s:4:\"\0*\0g\";b:0;}s:35:\"\0wandell\\Example\\Popo\\Popo2\0object2\";r:2;}";
-
     @Test
     void testSimpleSerialize() {
         Pojo1 pojo1 = new Pojo1();
@@ -18,7 +16,7 @@ public class SerializerTest {
             Serializer serializer = new Serializer();
             String serialized = serializer.serialize(pojo1);
 
-            assertEquals(serialized, SIMPLE_OBJECT);
+            assertEquals(serialized, OBJECT_SIMPLE);
         } catch (Exception.MissingPHPClassAnnotation missingPHPClassAnnotation) {
             fail();
         }
